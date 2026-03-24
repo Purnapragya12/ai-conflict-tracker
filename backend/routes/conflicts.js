195e9c0335da0399f787e12186f0c83dfbf67949
Purnapragya12 <122694140+Purnapragya12@users.noreply.github.com>
2026-03-24 17:44:03 +0530

    Update conflicts.js

--- backend/routes/conflicts.js
+++ backend/routes/conflicts.js
@@ -1,1 +1,64 @@
+const express =
+require("express");
 
+const getNews =
+require("../services/newsService");
+
+const summarize =
+require("../services/aiService");
+
+const risk =
+require("../services/riskService");
+
+const router =
+express.Router();
+
+router.get("/",async(req,res)=>{
+
+try{
+
+const news =
+await getNews();
+
+const processed =
+await Promise.all(
+
+news.map(async(item)=>({
+
+title:item.title,
+
+summary:
+await summarize(
+item.description
+),
+
+lat:
+20 + Math.random()*40,
+
+lng:
+-120 + Math.random()*240,
+
+risk:
+risk(),
+
+url:item.url
+
+}))
+
+);
+
+res.json(processed);
+
+}
+
+catch(error){
+
+console.log(error);
+
+res.json([]);
+
+}
+
+});
+
+module.exports=router;
